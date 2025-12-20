f5de6f18d1f065f16061011470dd5e6b5d314d91
Fabse120 <fabseH60@gmail.com>
2025-12-20 16:24:38 +0100

    Added Database.py for Database setup.

---
+++ app/Database.py
@@ -0,0 +1,97 @@
+import _sqlite3
+
+class Database:
+
+    @staticmethod
+    def establish_con(db_name):
+        return _sqlite3.connect("VoltCastDB")
+
+    def setup_db(self):
+        """
+        Called during Backend setup
+        :return:
+        """
+        con = self.establish_con("VoltCastDB")
+        cur = con.cursor()
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS user(
+                        username TEXT PRIMARY KEY NOT NULL,
+                        password TEXT);
+                    ''')
+        con.commit()
+
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS microservices(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        name TEXT NOT NULL,
+                        ip_address TEXT,
+                        port INTEGER,
+                        username TEXT,
+                        FOREIGN KEY(username) REFERENCES user(username));
+                    ''')
+        con.commit()
+        con.close()
+
+    def write_user_data(self, user):
+        con = self.establish_con("VoltCastDB")
+        cur = con.cursor()
+
+        # timestamp = realtime_data["realtime_data"]["current_generation"]["timestamp"]
+        # unit = realtime_data["realtime_data"]["current_generation"]["unit"]
+        # value = realtime_data["realtime_data"]["current_generation"]["value"]
+        #
+        # cur.execute('''INSERT INTO current_energy (timestamp, unit, energy)
+        #                VALUES (?, ?, ?)''',
+        #             (timestamp, unit, value))
+        # con.commit()
+        #
+        # timestamp = realtime_data["realtime_data"]["current_consumption"]["timestamp"]
+        # unit = realtime_data["realtime_data"]["current_consumption"]["unit"]
+        # value = realtime_data["realtime_data"]["current_consumption"]["value"]
+        #
+        # cur.execute('''INSERT INTO current_consumption (timestamp, unit, energy)
+        #                VALUES (?, ?, ?)''',
+        #             (timestamp, unit, value))
+        # con.commit()
+        #
+        # timestamp = realtime_data["realtime_data"]["battery_capacity"]["timestamp"]
+        # unit = realtime_data["realtime_data"]["battery_capacity"]["unit"]
+        # value = realtime_data["realtime_data"]["battery_capacity"]["value"]
+
+        # cur.execute('''INSERT INTO battery_capacity (timestamp, unit, energy)
+        #                VALUES (?, ?, ?)''',
+        #             (timestamp, unit, value))
+        con.commit()
+        con.close()
+
+        def write_microservice_data(self, microservice):
+            con = self.establish_con("VoltCast_DB")
+            cur = con.cursor()
+
+            # timestamp = realtime_data["realtime_data"]["current_generation"]["timestamp"]
+            # unit = realtime_data["realtime_data"]["current_generation"]["unit"]
+            # value = realtime_data["realtime_data"]["current_generation"]["value"]
+            #
+            # cur.execute('''INSERT INTO current_energy (timestamp, unit, energy)
+            #                VALUES (?, ?, ?)''',
+            #             (timestamp, unit, value))
+            # con.commit()
+            #
+            # timestamp = realtime_data["realtime_data"]["current_consumption"]["timestamp"]
+            # unit = realtime_data["realtime_data"]["current_consumption"]["unit"]
+            # value = realtime_data["realtime_data"]["current_consumption"]["value"]
+            #
+            # cur.execute('''INSERT INTO current_consumption (timestamp, unit, energy)
+            #                VALUES (?, ?, ?)''',
+            #             (timestamp, unit, value))
+            # con.commit()
+            #
+            # timestamp = realtime_data["realtime_data"]["battery_capacity"]["timestamp"]
+            # unit = realtime_data["realtime_data"]["battery_capacity"]["unit"]
+            # value = realtime_data["realtime_data"]["battery_capacity"]["value"]
+            #
+            # cur.execute('''INSERT INTO battery_capacity (timestamp, unit, energy)
+            #                VALUES (?, ?, ?)''',
+            #             (timestamp, unit, value))
+            con.commit()
+            con.close()
